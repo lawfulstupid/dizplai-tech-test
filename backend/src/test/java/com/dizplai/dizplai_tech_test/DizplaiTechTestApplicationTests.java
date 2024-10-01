5d9d2160820a09be86fc0bcb988bffbed3007a1a
Alex Bradley <a.bradley.dm@gmail.com>
2024-10-01 17:58:38 +0100

    Java+Spring backend init

---
+++ backend/src/test/java/com/dizplai/dizplai_tech_test/DizplaiTechTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizplai.dizplai_tech_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DizplaiTechTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
