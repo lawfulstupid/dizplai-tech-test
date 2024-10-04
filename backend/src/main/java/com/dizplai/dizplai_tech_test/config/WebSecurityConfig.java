cbb63d6f26abd925eb31a1dadf2830977af31c9b
Alex Bradley <a.bradley.dm@gmail.com>
2024-10-04 12:19:26 +0100

    Docker compose to combine RDS and App

--- backend/src/main/java/com/dizplai/dizplai_tech_test/config/WebSecurityConfig.java
+++ backend/src/main/java/com/dizplai/dizplai_tech_test/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,7 +24,7 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/poll/**").permitAll()
                         .requestMatchers("/cookie").permitAll()
