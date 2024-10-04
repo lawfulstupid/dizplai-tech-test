f7142cec7891268b00710ad5084b670df63f0675
Alex Bradley <a.bradley.dm@gmail.com>
2024-10-04 13:40:43 +0100

    Exception handling

---
+++ backend/src/main/java/com/dizplai/dizplai_tech_test/config/ExceptionResolver.java
@@ -0,0 +1,19 @@
+package com.dizplai.dizplai_tech_test.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ExceptionResolver {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public void handleIllegalArgument() {}
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(value = IllegalCallerException.class)
+    public void handleIllegalCaller() {}
+
+}
